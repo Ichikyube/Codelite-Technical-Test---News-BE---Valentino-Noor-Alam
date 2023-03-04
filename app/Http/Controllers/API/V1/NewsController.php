b82ffc15dc4f9562554c2f6ca64b55bf9e2d492d
ichikyube <ichikyube@gmail.com>
2023-03-04 07:38:46 +0700

    joss

--- app/Http/Controllers/API/V1/NewsController.php
+++ app/Http/Controllers/API/V1/NewsController.php
@@ -81,7 +81,7 @@ class NewsController extends Controller
         if ($validated['banner']) {
             $banner = $request->file('banner');
             $imageName = date('YmdHis') . "." . $banner->getClientOriginalName();
-            $path =  $request->getSchemeAndHttpHost() . '/storage/' . $banner->storeAs('img',$imageName);
+            $path =  $request->getSchemeAndHttpHost() . '/' . $banner->storeAs('images',$imageName);
             $validated['banner'] = $path;
         } else
         {
